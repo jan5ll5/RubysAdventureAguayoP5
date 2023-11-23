cafb3223578d82aa3ab7f38380cf1b8ac1ac0acf
janelle <145067236+jan5ll5@users.noreply.github.com>
2023-11-22 17:53:42 -0800

    Updated all the scripts and started cinemachine.
    I updated all the scripts so they can all match each other and be correct.

--- RubysAdventureP5Aguayo/Assets/Scripts/Projectile.cs
+++ RubysAdventureP5Aguayo/Assets/Scripts/Projectile.cs
@@ -6,24 +6,36 @@ public class Projectile : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody2D rigidbody2d;
+
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody>();
+    }
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>(); 
     }
 
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        //we also add a debug log to know what the projectile touch
-        Debug.Log("Projectile Collision with " + other.gameObject);
-        Destroy(gameObject);
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void Launch(Vector2 direction, float force)
     {
         rigidbody2d.AddForce(direction * force);
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+        if (e != null)
+        {
+            e.Fix();
+        }
+        Destroy(gameObject);
     }
 }
--- RubysAdventureP5Aguayo/Assets/Scripts/RubyController.cs
+++ RubysAdventureP5Aguayo/Assets/Scripts/RubyController.cs
@@ -7,11 +7,12 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
-    public float timeInvincible = 2.0f;
+    public GameObject projectilePrefab;
 
     public int health { get { return currentHealth; } }
     int currentHealth;
 
+    public float timeInvincible = 2.0f;
     bool isInvincible;
     float invincibleTimer;
 
@@ -55,6 +56,10 @@ public class RubyController : MonoBehaviour
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
     }
 
     void FixedUpdate()
@@ -83,7 +88,12 @@ public class RubyController : MonoBehaviour
     }
     void Launch()
     {
-        GameObject projectile O
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
     }
 }
 
