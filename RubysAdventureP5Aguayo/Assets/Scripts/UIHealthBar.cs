69a59bf3ca489f78ddf11f96117677aa9364f8cd
janelle <145067236+jan5ll5@users.noreply.github.com>
2023-11-25 15:01:08 -0800

    fixed the health bar and added script to it
    I fixed the health bar so it could be the correct size and added script to the health bar.

---
+++ RubysAdventureP5Aguayo/Assets/Scripts/UIHealthBar.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHealthBar : MonoBehaviour
+{
+    public static UIHealthBar instance { get; private set; }
+
+    public Image mask;
+    float organizer;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalSize = mask.rectTransform.rect.width;
+
+    }
+
+    public void SetValue(float value)
+    {
+        mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize * value);
+    }
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
