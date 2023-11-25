776e260f10e6f7f09f4422f91fb93794f9e84059
janelle <145067236+jan5ll5@users.noreply.github.com>
2023-11-24 22:08:59 -0800

    fixed animations and added particles
    I fixed the robot animations and projectile to match with Ruby.

--- RubysAdventureP5Aguayo/Assets/Scripts/Projectile.cs
+++ RubysAdventureP5Aguayo/Assets/Scripts/Projectile.cs
@@ -9,11 +9,8 @@ public class Projectile : MonoBehaviour
     // Start is called before the first frame update
     
 
+
     void Awake()
-    {
-        rigidbody2d = GetComponent<Rigidbody2D>();
-    }
-    void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
